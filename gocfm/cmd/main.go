02b29f77a6bc63e4eed1b643a69154bc6b7d65de
LiangYaHui <46517115@qq.com>
2023-01-30 10:08:29 +0800

    umijs要不要脸，捆绑那么多阿里的东西

--- gocfm/cmd/main.go
+++ gocfm/cmd/main.go
@@ -6,7 +6,6 @@ import (
 
 	"github.com/ryouaki/gocfm"
 	"github.com/ryouaki/gocfm/controller"
-	"github.com/ryouaki/gocfm/render"
 	"github.com/ryouaki/koa"
 	"github.com/ryouaki/koa/session"
 	"github.com/ryouaki/koa/static"
@@ -15,9 +14,7 @@ import (
 func main() {
 	app := koa.New()
 
-	if os.Getenv("MODE") == "DEV" {
-		app.Use(static.Static("../static", "/static/"))
-	} else {
+	if os.Getenv("MODE") != "DEV" {
 		app.Use(static.Static("./static", "/static/"))
 	}
 
@@ -27,17 +24,7 @@ func main() {
 	app.Use(session.Session(sessConf, gocfm.SessionStore))
 
 	app.Use(func(ctx *koa.Context, next koa.Next) {
-		isLogin := ctx.GetCookie("isLogin")
-		if isLogin == nil {
-			ctx.Status = 401
-			tpl, err := render.RenderLogin(ctx)
-			if err != nil {
-				fmt.Println("111", err)
-			}
-			ctx.SetBody(tpl)
-		} else {
-			next()
-		}
+
 	})
 	controller.InitController(app)
 
