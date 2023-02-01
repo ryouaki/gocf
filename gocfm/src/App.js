b3aa76f926a37c3a7044bd03be2190abd5e8ee2c
LiangYaHui <46517115@qq.com>
2023-02-01 19:25:20 +0800

    feat: 移除redux

--- gocfm/src/App.js
+++ gocfm/src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router';
 import './App.css';
+
+import Login from './page/login';
+import List from './page/list';
+import Edit from './page/edit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/list" element={<List />} />
+      <Route path="/edit" element={<Edit />} />
+    </Routes>
   );
 }
 
