cd27fbc82368cbd1b5848ca346121f69ee60357f
LiangYaHui <46517115@qq.com>
2023-02-03 17:14:45 +0800

    前端开发完毕

--- gocfm/src/index.js
+++ gocfm/src/index.js
@@ -8,11 +8,9 @@ import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from './store/user';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <UserProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </UserProvider>
-  </React.StrictMode>
+  <UserProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </UserProvider>
 );
--- gocfm/src/page/edit.js
+++ gocfm/src/page/edit.js
@@ -1,19 +1,31 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router";
 
 export default function Edit () {
-  let editor = useRef(null)
+  let editor = useRef(null);
+  let nav = useNavigate()
+  let [edit, setEdit] = useState(null)
 
   useEffect(() => {
-    window.monaco.editor.create(editor.current, {
+    const edit = window.monaco.editor.create(editor.current, {
       language: 'javascript'
     });
-  })
-  return <section className="editor">
-    <div>
+    setEdit(edit);
+  }, [])
 
+  function pushCode() {
+    console.log(edit.getValue())
+  }
+  return <section className="editor pr">
+    <div className="df">
+      <div className="file_name">main.js <span onClick={() => {
+        nav(-1);
+      }}>X</span></div>
     </div>
     <div ref={editor} id="editor">
-      
     </div>
+    <div className="push-btn pa" onClick={() => {
+      pushCode()
+    }}>发布</div>
   </section>
 }
